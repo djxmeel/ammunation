186cb5c2e90f90b37d26383e4eaecaee81a6cb45
oniiyhivi <djamelnadour15@gmail.com>
2022-02-27 21:45:56 +0100

    main content removed from header

--- index/index.php
+++ index/index.php
@@ -10,5 +10,11 @@
     <title>WELCOME TO AMMUNATION</title>
 </head>
 <?php require_once("../modules/header.php") ?>
+    <main class="home_content">
+        <h1>WELCOME TO AMMUNATION <?php echo strtoupper($_SESSION["username"]) ?> <i class='bx bx-cross'></i></h1>
+    </main>
+    <form action="" method="POST" style="float: right;">
+        <input type="submit" value="Logout" name="logout">
+    </form>
 </body>
 </html>
--- modules/header.php
+++ modules/header.php
@@ -84,10 +84,4 @@
             </div>
         </div>
     </div>
-    <main class="home_content">
-        <h1>WELCOME TO AMMUNATION <?php echo strtoupper($_SESSION["username"]) ?> <i class='bx bx-cross'></i></h1>
-    </main>
-    <form action="" method="POST" style="float: right;">
-        <input type="submit" value="Logout" name="logout">
-    </form>
     <script src="../js/sidebar.js"></script>
