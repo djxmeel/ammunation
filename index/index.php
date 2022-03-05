2f04ea39b142347624c54a2940f6bd3a40da7d73
oniiyhivi <djamelnadour15@gmail.com>
2022-03-05 14:15:41 +0100

    Optimized header module structure

--- index/index.php
+++ index/index.php
@@ -1,15 +1,5 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../css/sidebar.css">
-    <link rel="stylesheet" href="../css/style.css">
-    <link href='https://unpkg.com/boxicons@2.1.1/css/boxicons.min.css' rel='stylesheet'>
-    <title>WELCOME TO AMMUNATION</title>
-</head>
 <?php require_once("../modules/header.php") ?>
+<title>WELCOME TO AMMUNATION</title>
     <main class="home_content">
         <h1>WELCOME TO AMMUNATION <?php echo strtoupper($_SESSION["username"]) ?> <i class='bx bx-cross'></i></h1>
     </main>
--- purchases/purchases_list.php
+++ purchases/purchases_list.php
@@ -1,14 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../css/sidebar.css">
-    <link rel="stylesheet" href="../css/style.css">
-    <link href='https://unpkg.com/boxicons@2.1.1/css/boxicons.min.css' rel='stylesheet'>
-    <title>WELCOME TO AMMUNATION</title>
-</head>
+<title>WELCOME TO AMMUNATION</title>
 <?php require_once("../modules/header.php") ?>
 </body>
 </html>
