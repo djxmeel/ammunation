11c01fee388618c20698c2960fb5966ca56763eb
oniiyhivi <djamelnadour15@gmail.com>
2022-03-06 01:54:36 +0100

    Made chart funcional

--- index/index.php
+++ index/index.php
@@ -1,57 +1,18 @@
 <?php require_once("../modules/header.php");
 require_once("../modules/sql.php");?>
 <title>WELCOME TO AMMUNATION</title>
-    <main class="home_content">
-        <h1>WELCOME TO AMMUNATION <?php echo strtoupper($_SESSION["username"]) ?> <i class='bx bx-cross'></i></h1>
-        <section class="col-12 col-sm-6">
-            <div>
-                <h1>Products</h1>
-                <canvas id="myChart"></canvas>
-            </div>
-        </section>
-        <section class="col-12 col-sm-6">
+<main class="home_content">
+    <h1>WELCOME TO AMMUNATION <?php echo strtoupper($_SESSION["username"]) ?> <i class='bx bx-cross'></i></h1>
+    <section class="col-12 col-sm-6">
+        <div>
+            <h1>Products</h1>
+            <canvas id="myChart"></canvas>
+        </div>
+    </section>
+    <section class="col-12 col-sm-6">
         
         </section>
     </main>
-    <script>
-        const data = {
-            labels: [
-                'ARs',
-                'SMGs',
-                'Shotguns',
-                'LMGs',
-                'Snipers',
-                'Handguns'
-            ],
-            datasets: [{
-                label: 'Categories',
-                data: [20, 20, 20, 20, 20, 20],
-                backgroundColor: [
-                '#4b4453',
-                '#ff0000',
-                '#845ec2',
-                '#4ffbdf',
-                '#ff8066',
-                '#4e8397'
-                ],
-                hoverOffset: 4
-            }]
-        };
-        
-        const config = {
-            type: 'doughnut',
-            data: data,
-            };
-        
-        const myChart = new Chart(
-        document.getElementById('myChart'),
-        config
-        );
-
-        const myChart2 = new Chart(
-        document.getElementById('myChart2'),
-        config
-        );
-    </script>
+    <?php require_once("../modules/chart_data.php");?>
 </body>
 </html>
--- modules/chart_data.php
+++ modules/chart_data.php
@@ -1,5 +1,55 @@
 <?php
     require_once("sql.php");
 
-    $query = "SELECT COUNT(id_categoria) AS count FROM productos GROUP BY id_categoria ORDER BY id_categoria "
+    $query = "SELECT COUNT(id_categoria) AS count FROM productos GROUP BY id_categoria ORDER BY id_categoria";
+
+    $result = $con->query($query);
+
+    $stats = ",";
+
+    while($row = $result->fetch_assoc()){
+        $stats = $stats.",".$row["count"];
+    }
+
+    $stats = trim($stats, ",");
 ?>
+<script>
+        const data = {
+            labels: [
+                'ARs',
+                'SMGs',
+                'Shotguns',
+                'LMGs',
+                'Snipers',
+                'Handguns'
+            ],
+            datasets: [{
+                label: 'Categories',
+                data: [<?php echo $stats ?>],
+                backgroundColor: [
+                '#4b4453',
+                '#ff0000',
+                '#845ec2',
+                '#4ffbdf',
+                '#ff8066',
+                '#4e8397'
+                ],
+                hoverOffset: 4
+            }]
+        };
+        
+        const config = {
+            type: 'doughnut',
+            data: data,
+            };
+        
+        const myChart = new Chart(
+        document.getElementById('myChart'),
+        config
+        );
+
+        const myChart2 = new Chart(
+        document.getElementById('myChart2'),
+        config
+        );
+    </script>
