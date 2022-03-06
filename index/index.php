556227f881965d5f2d7f53e9eee3cc1b3172c5dd
oniiyhivi <djamelnadour15@gmail.com>
2022-03-06 15:27:35 +0100

    removed chart

--- index/index.php
+++ index/index.php
@@ -1,18 +1,10 @@
 <?php require_once("../modules/header.php");
 require_once("../modules/sql.php");?>
-<title>WELCOME TO AMMUNATION</title>
+
 <main class="home_content">
-    <h1>WELCOME TO AMMUNATION <?php echo strtoupper($_SESSION["username"]) ?> <i class='bx bx-cross'></i></h1>
-    <section class="col-12 col-sm-6">
-        <div>
-            <h1>Products</h1>
-            <canvas id="myChart"></canvas>
-        </div>
-    </section>
-    <section class="col-12 col-sm-6">
-        
-        </section>
-    </main>
-    <?php require_once("../modules/chart_data.php");?>
+    <h1>WELCOME TO AMMUNATION <?php echo strtoupper($_SESSION["username"]) ?> <i class='bx bxs-star'></i></h1>
+    
+ 
+</main>
 </body>
 </html>
--- modules/chart_data.php
+++ modules/chart_data.php
@@ -13,38 +13,3 @@
 
     $stats = trim($stats, ",");
 ?>
-<script>
-        const data = {
-            labels: [
-                'ARs',
-                'SMGs',
-                'Shotguns',
-                'LMGs',
-                'Snipers',
-                'Handguns'
-            ],
-            datasets: [{
-                label: 'Categories',
-                data: [<?php echo $stats ?>],
-                backgroundColor: [
-                '#4b4453',
-                '#ff0000',
-                '#845ec2',
-                '#4ffbdf',
-                '#ff8066',
-                '#4e8397'
-                ],
-                hoverOffset: 4
-            }]
-        };
-        
-        const config = {
-            type: 'doughnut',
-            data: data,
-            };
-        
-        const myChart = new Chart(
-        document.getElementById('myChart'),
-        config
-        );
-    </script>
